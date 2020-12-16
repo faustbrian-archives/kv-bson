a15dd57c6bb06a7b36e4dff00b4e52835176fa6a
Brian Faust <hello@basecode.sh>
2020-12-16 13:22:54 +0200

    first commit

---
+++ src/sync.ts
@@ -0,0 +1,22 @@
+// tslint:disable: no-unsafe-any
+import { StoreSync as AbstractStore } from "@konceiver/kv-file";
+import { deserialize, serialize } from "bson";
+import { readFileSync, writeFileSync } from "fs-extra";
+
+export class StoreSync<K, T> extends AbstractStore<K, T> {
+	public static new<K, T>(uri: string): StoreSync<K, T> {
+		return new StoreSync<K, T>(new Map<K, T>(), uri);
+	}
+
+	// @ts-ignore
+	protected dump(rows: Record<K, T>): void {
+		writeFileSync(this.uri, serialize(rows));
+	}
+
+	protected load(): void {
+		for (const [key, value] of Object.entries(deserialize(readFileSync(this.uri)))) {
+			// @ts-ignore
+			this.put(key, value);
+		}
+	}
+}
